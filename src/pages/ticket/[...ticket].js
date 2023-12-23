c0e87a6131dac3a7d37a19d555256e2b926e28f6
Amil Silahic <jinobie02@gmail.com>
2023-12-22 21:55:16 -0700

    creating few more pages and getting components started

---
+++ src/pages/ticket/[...ticket].js
@@ -0,0 +1,5 @@
+const TicketPage = () => {
+  return <div>ticket page</div>;
+};
+
+export default TicketPage;
---
+++ src/pages/ticket/index.js
@@ -0,0 +1,9 @@
+const MainTicketPage = () => {
+  return (
+    <div>
+      <h1>Ticket Page</h1>
+    </div>
+  );
+};
+
+export default MainTicketPage;
